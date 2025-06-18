2e4d2733bb183a961502500f1c4f81bcf3fae852
Hadrien Pulcini <hpulcini @free.fr>
2025-06-18 10:20:10 +0200

    fixed size suggestions

--- Home.py
+++ Home.py
@@ -124,7 +124,7 @@ with main_columns[1]:
 
                 for i in range(nb_images):
                     with columns[i]:
-                        st.image(sample_images[i], caption=f"{sorted_breeds[i][0]}, {sorted_breeds[i][1]* 100:.0f} %", use_container_width=True)
+                        st.image(sample_images[i], caption=f"{sorted_breeds[i][0]}, {sorted_breeds[i][1]* 100:.0f} %", use_container_width=False)
                 # ----- BREED CHART -----
                 st.markdown("### Confidence by Breed")
                 st.altair_chart(bar_chart, use_container_width=True)
--- toolkit.py
+++ toolkit.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 def get_sample_image(breed, sample_image_folder="breeds_sample_images"):
     """
     Returns a sample image for the input breed.
@@ -5,4 +8,5 @@ def get_sample_image(breed, sample_image_folder="breeds_sample_images"):
     filename = breed.lower()
     filename = " ".join(filename.split("-"))
     filename = "_".join(filename.split(" "))
-    return f"{sample_image_folder}/{filename}.jpg"
+    image = Image.open(f"{sample_image_folder}/{filename}.jpg")
+    return image.resize((300, 300))
