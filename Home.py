4f72ec4442f477c7ecbe2f46b87526ac51901d4b
Hadrien Pulcini <hpulcini @free.fr>
2025-06-18 10:50:17 +0200

    always 3, no threshold

--- Home.py
+++ Home.py
@@ -116,15 +116,14 @@ with main_columns[1]:
                     titleFontSize=16
                 )
 
-                relevant_breeds = [(breed, value) for breed, value in sorted_breeds if value > 0.05]
-                nb_images = min(len(relevant_breeds), 3)
-                columns = st.columns([1 for _ in range(3)])
+                columns = st.columns([1, 1, 1])
 
                 sample_images = [get_sample_image(tup[0]) for tup in sorted_breeds[:3]]
 
-                for i in range(nb_images):
+                for i, sample_image in enumerate(sample_images):
                     with columns[i]:
-                        st.image(sample_images[i], caption=f"{sorted_breeds[i][0]}, {sorted_breeds[i][1]* 100:.0f} %", use_container_width=True)
+                        st.image(sample_image, caption=f"{sorted_breeds[i][0]}, {sorted_breeds[i][1]* 100:.0f} %", use_container_width=True)
+
                 # ----- BREED CHART -----
                 st.markdown("### Confidence by Breed")
                 st.altair_chart(bar_chart, use_container_width=True)
